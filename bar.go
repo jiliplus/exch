8c03cfcda619035f1ce111415470fae26efe306f
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-04-29 22:05:36 +0800

    开始编写 GenBar 函数

--- bar.go
+++ bar.go
@@ -13,27 +13,68 @@ type Bar struct {
 	// 为了节约空间，不要保存在数据库中
 	Symbol   string
 	Exchange Name
+	// TODO: Interval 有必要吗
 	Interval time.Duration
 }
 
-func newBar(begin time.Time, ticks []float64) Bar {
-	open, high, low := ticks[0], ticks[0], ticks[0]
-	close := ticks[len(ticks)-1]
-	for _, t := range ticks {
-		high = maxFloat64(high, t)
-		low = minFloat64(low, t)
-	}
+func newBar(tick *Tick, date time.Time) Bar {
 	return Bar{
-		Begin: begin,
-		Open:  open,
-		High:  high,
-		Low:   low,
-		Close: close,
+		Begin:  date,
+		Open:   tick.Price,
+		High:   tick.Price,
+		Low:    tick.Price,
+		Close:  tick.Price,
+		Volume: tick.Volume,
 	}
 }
 
-// TODO: 完成这个闭包函数
-func barGenFunc() {
+func newEmptyBar(bar *Bar, date time.Time) Bar {
+	return Bar{
+		Begin:  date,
+		Open:   bar.Close,
+		High:   bar.Close,
+		Low:    bar.Close,
+		Close:  bar.Close,
+		Volume: 0,
+	}
+}
 
-	return
+// GenBarFunc 会返回一个接收 tick 并生成 bar 的闭包函数
+// TODO: 完成这个闭包函数
+// 有以下情况需要处理
+// 1. 接收第一个 tick,
+//    不返回 bar
+// 2. 接收到当前的 interval 的 tick
+//    不返回 bar
+// 3. 接收到下一个 interval 的 tick
+//    返回上一个 bar
+// 4. 接收到下一个 interval 后面的 interval 的 tick，市场冷清，长时间没有交易
+//    返回多个 bar
+func GenBarFunc(begin BeginFunc, interval time.Duration) func(*Tick) []Bar {
+	isInited := false
+	var bar Bar
+	var next time.Time
+	return func(tick *Tick) []Bar {
+		tickBegin := begin(tick.Date, interval)
+		if !isInited {
+			bar = newBar(tick, tickBegin)
+			next = bar.Begin.Add(interval)
+			isInited = true
+			return nil
+		}
+
+		if tickBegin.Before(next) {
+			bar.High = maxFloat64(bar.High, tick.Price)
+			bar.Low = minFloat64(bar.Low, tick.Price)
+			bar.Close = tick.Price
+			bar.Volume += tick.Volume
+			return nil
+		}
+
+		res := make([]Bar, 0, 256)
+		for next.Before(tickBegin) {
+			next = next.Add(interval)
+		}
+
+	}
 }
