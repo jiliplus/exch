f49af9a17394d9dfc585acc55bc0dc6b2ab8db88
aQuaYi.com <6028869+aQuaYi@users.noreply.github.com>
2020-05-22 11:56:49 +0800

    添加了 BalanceService

--- backtest/balanceService.go
+++ backtest/balanceService.go
@@ -6,47 +6,73 @@ import (
 	"log"
 	"time"
 
-	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/jujili/clock"
 	"github.com/jujili/exch"
 )
 
 // BalanceService 会在每天的凌晨零点零分零秒记录 balance 的总价值
-// TODO: 从这里开始
-func BalanceService(ctx context.Context, ps pubsub, interval time.Duration) {
-	topic := fmt.Sprintf("%sBar", interval)
-	log.Printf(`从 tick 生成的 bar 会发送到 "%s" 话题中`, topic)
-	//
+// prices 里面需要放好各种资产的价格，不要忘记 capital 的价格是 1
+func BalanceService(ctx context.Context, ps pubsub, prices map[string]float64, asset string) {
 	ticks, err := ps.Subscribe(ctx, "tick")
 	if err != nil {
 		panic(err)
 	}
 	decTick := exch.DecTickFunc()
 	//
-	gtb := exch.GenTickBarFunc(exch.Begin, interval)
-	//
-	enc := exch.EncFunc()
-	//
-	var bars []exch.Bar
-	//
+	balances, err := ps.Subscribe(ctx, "balance")
+	if err != nil {
+		panic(err)
+	}
+	decBal := exch.DecBalanceFunc()
+	// 创建模拟 clock
+	msg := <-ticks
+	tick := decTick(msg.Payload)
+	prices[asset] = tick.Price
+	clock := clock.NewSimulator(tick.Date)
+	everyNewDay := clock.EveryDay(0, 0, 0)
+	// 另起一个 goroutine，更新 clock
 	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				log.Fatalln("TickBarService Down: ", ctx.Err())
-			case msg, ok := <-ticks:
-				msg.Ack()
-				if !ok {
-					bars = gtb(exch.NilTick)
-				} else {
-					tick := decTick(msg.Payload)
-					bars = gtb(tick)
-				}
-				for _, bar := range bars {
-					msg := message.NewMessage(watermill.NewUUID(), enc(bar))
-					ps.Publish(topic, msg)
-				}
-			}
+		tks, _ := ps.Subscribe(ctx, "tick")
+		for msg := range tks {
+			tick := decTick(msg.Payload)
+			clock.SetOrPanic(tick.Date)
+			// 设置好了才确认
+			msg.Ack()
 		}
 	}()
+	//
+	go func() {
+		var msg *message.Message
+		var bal *exch.Balance
+		bs := make([]balanceSnap, 0, 2048)
+		ok := true
+		for ok {
+			select {
+			case <-ctx.Done():
+				log.Fatalln("BalanceService Down: ", ctx.Err())
+			case msg, ok = <-ticks:
+				tick := decTick(msg.Payload)
+				prices[asset] = tick.Price
+			case msg = <-balances:
+				bal = decBal(msg.Payload)
+				msg.Ack()
+			case date := <-everyNewDay:
+				bs = append(bs, newBalanceSnap(date, bal, prices))
+			}
+		}
+		fmt.Println(bs)
+	}()
+}
+
+type balanceSnap struct {
+	date   time.Time
+	amount float64
+}
+
+func newBalanceSnap(date time.Time, balance *exch.Balance, prices map[string]float64) balanceSnap {
+	return balanceSnap{
+		date:   date,
+		amount: balance.Total(prices),
+	}
 }
