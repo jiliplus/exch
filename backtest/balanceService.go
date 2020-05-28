570d5044a7b97e083201894663ce3bc30f8f3c84
aQuaYi.com <6028869+aQuaYi@users.noreply.github.com>
2020-05-28 10:58:58 +0800

    修复一处并发 bug

--- backtest/balanceService.go
+++ backtest/balanceService.go
@@ -25,45 +25,47 @@ func BalanceService(ctx context.Context, ps Pubsub, prices map[string]float64, a
 		panic(err)
 	}
 	decBal := exch.DecBalanceFunc()
-	// 创建模拟 clock
-	msg := <-ticks
-	tick := decTick(msg.Payload)
-	prices[asset] = tick.Price
-	clock := clock.NewSimulator(tick.Date)
-	everyNewDay := clock.EveryDay(0, 0, 0)
-	// 另起一个 goroutine，更新 clock
 	go func() {
-		tks, _ := ps.Subscribe(ctx, "tick")
-		for msg := range tks {
-			tick := decTick(msg.Payload)
-			clock.SetOrPanic(tick.Date)
-			// 设置好了才确认
-			msg.Ack()
-		}
-	}()
-	//
-	go func() {
-		var msg *message.Message
-		var bal *exch.Balance
-		bs := make([]balanceSnap, 0, 2048)
-		ok := true
-		for ok {
-			select {
-			case <-ctx.Done():
-				log.Fatalln("BalanceService Down: ", ctx.Err())
-			case msg, ok = <-ticks:
+		// 创建模拟 clock
+		msg := <-ticks
+		tick := decTick(msg.Payload)
+		prices[asset] = tick.Price
+		clock := clock.NewSimulator(tick.Date)
+		everyNewDay := clock.EveryDay(0, 0, 0)
+		// 另起一个 goroutine，更新 clock
+		go func() {
+			tks, _ := ps.Subscribe(ctx, "tick")
+			for msg := range tks {
 				tick := decTick(msg.Payload)
-				prices[asset] = tick.Price
-			case msg = <-balances:
-				bal = decBal(msg.Payload)
+				clock.SetOrPanic(tick.Date)
+				// 设置好了才确认
 				msg.Ack()
-			case date := <-everyNewDay:
-				newBal := newBalanceSnap(date, bal, prices)
-				bs = append(bs, newBal)
-				log.Println(date, newBal)
 			}
-		}
-		fmt.Println(bs)
+		}()
+		//
+		go func() {
+			var msg *message.Message
+			var bal *exch.Balance
+			bs := make([]balanceSnap, 0, 2048)
+			ok := true
+			for ok {
+				select {
+				case <-ctx.Done():
+					log.Fatalln("BalanceService Down: ", ctx.Err())
+				case msg, ok = <-ticks:
+					tick := decTick(msg.Payload)
+					prices[asset] = tick.Price
+				case msg = <-balances:
+					bal = decBal(msg.Payload)
+					msg.Ack()
+				case date := <-everyNewDay:
+					newBal := newBalanceSnap(date, bal, prices)
+					bs = append(bs, newBal)
+					log.Println(date, newBal)
+				}
+			}
+			fmt.Println(bs)
+		}()
 	}()
 }
 
